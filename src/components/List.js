978a030a573d619cf2f3ab94e3ab1bcbcbcad331
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-09-02 15:14:08 +0800

    integrated dummy data into List.js and Card.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { users } from "../data";
+import Card from "./Card";
 
 class List extends React.Component {
   render() {
     return (
-      <div>
-        <h2>List</h2>
+      <div style={{ margin: "0 auto" }}>
+        {users.map(
+          ({ username, imageUrl, caption, likes, timestamp, comments }) => {
+            return (
+              <Card
+                username={username}
+                post={imageUrl}
+                caption={caption}
+                likes={likes}
+                timestamp={timestamp}
+                comments={comments}
+              />
+            );
+          }
+        )}
       </div>
     );
   }
